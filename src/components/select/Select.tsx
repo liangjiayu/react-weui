42931a7e7af9b78e3a34777bcdd09f0428d5719a
liangjiayu <1013834609@qq.com>
2022-09-14 23:03:58 +0800

    feat: select选择框

--- src/components/select/Select.tsx
+++ src/components/select/Select.tsx
@@ -1,22 +1,40 @@
 import type { SelectHTMLAttributes } from 'react';
 import React from 'react';
-import {} from '../../utils';
+import classNames from 'classnames';
+import { useMergedState } from '../../utils';
 
 export type SelectProps = {
   value?: string;
-  onChange?: (event: React.ChangeEvent<HTMLSelectElement>) => void;
   defaultValue?: string;
+  onChange?: (event: React.ChangeEvent<HTMLSelectElement>) => void;
 } & SelectHTMLAttributes<HTMLSelectElement>;
 
-const Select: React.FC<SelectProps> = () => {
+const Select: React.FC<SelectProps> = (props) => {
+  const { children, onChange, className, ...restProps } = props;
+
+  const [value, setValue] = useMergedState<string>({
+    value: props.value,
+    defaultValue: props.defaultValue,
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    if (props.value === undefined) {
+      setValue(e.target.value);
+    }
+    onChange?.(e);
+  };
+
+  const classes = classNames('weui-select', className);
+
   return (
-    <div className="weui-cell weui-cell_active weui-cell_select">
-      <div className="weui-cell__bd">
-        <select className="weui-select">
-          <option value="1">1</option>
-        </select>
-      </div>
-    </div>
+    <select
+      {...restProps}
+      className={classes}
+      onChange={handleChange}
+      value={value}
+    >
+      {children}
+    </select>
   );
 };
 
--- src/components/select/index.md
+++ src/components/select/index.md
@@ -1,16 +1,12 @@
 # Select 选择框
 
-<code src="./demos/index.tsx"></code>
+<code src="./demos/demo1.tsx"></code>
 
 ## API
 
-| 参数         | 说明                                         | 类型         | 默认值 |
-| ------------ | -------------------------------------------- | ------------ | ------ |
-| type         | 类型，支持原生 input 标签的 type 属性        | `string`     | `text` |
-| clearable    | 是否启用清除图标，点击清除图标后会清空输入框 | `boolean`    | -      |
-| defaultValue | 默认值                                       | `string`     | -      |
-| disabled     | 是否禁用                                     | `boolean`    | -      |
-| onChange     | 输入框内容变化时触发                         | `() => void` | -      |
-| value        | 输入值                                       | `string`     | -      |
-| placeholder  | 提示文本                                     | `string`     | -      |
-| readOnly     | 是否只读                                     | `boolean`    | -      |
+| 参数         | 说明                 | 类型              | 默认值 |
+| ------------ | -------------------- | ----------------- | ------ |
+| defaultValue | 默认值               | `string`          | -      |
+| value        | 选择框值             | `string`          | -      |
+| onChange     | 选择框内容变化时触发 | `() => void`      | -      |
+| children     | 原生`option`         | `React.ReactNode` | -      |

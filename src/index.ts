401718a3046eed51373a44a7577bdd19f6d0362e
liangjiayu <1013834609@qq.com>
2022-09-17 10:12:10 +0800

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import './global';
 
-export { default as Button } from './components/Button';
+export { default as Button } from './components/button';
 export { default as Icon } from './components/Icon';
 export { default as Cells } from './components/Cells';
 export { Row, Col } from './components/Flex';
